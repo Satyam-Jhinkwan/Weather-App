1604e85dbd42f8f64c935edd2b507a3139697076
satyam-jhinkwan <satyamjhinkwan07@gmail.com>
2025-02-14 16:32:06 +0530

    Add local Storage

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const apiKey = "a60a740213bfb4252eb902b1c10d4ace";
 const weatherInfo = document.getElementById("weatherInfo");
 
-async function getWeather() {
-  const city = document.getElementById("cityInput").value;
-  if (city === "") {
-    weatherInfo.innerHTML = "<p>Please Enter A City Name</p>";
-    return;
+async function getWeather(city) {
+  if (!city) {
+    const cityInput = document.getElementById("cityInput").value;
+    if (cityInput === "") {
+      weatherInfo.innerHTML = "<p>Please Enter A City Name</p>";
+      return;
+    }
+    city = cityInput;
+    localStorage.setItem("lastCity", city);
   }
+  if (city === "") {
+    city = cityInput.value;
+    if (city === "") {
+      weatherInfo.innerHTML = "<p>Please Enter A City Name</p>";
+      return;
+    }
+    localStorage.setItem("lastCity", city);
+  }
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   try {
@@ -22,7 +35,17 @@ async function getWeather() {
               <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
       `;
     weatherInfo.innerHTML = weatherHTML;
+    document.getElementById("cityInput").value = city;
   } catch (error) {
     weatherInfo.innerHTML = `<p>${error.message}</p>`;
   }
 }
+
+window.onload = function () {
+  const lastCity = localStorage.getItem("lastCity");
+  const cityInput = document.getElementById("cityInput");
+  if (lastCity) {
+    cityInput.value = lastCity;
+    getWeather(lastCity);
+  }
+};
